41f40857f9646733a227158123713a10167e3c10
et2010 <jianwang.academic@gmail.com>
2018-05-27 15:04:12 +0800

    scimax-org: add avy as dependency; add package file header

--- scimax-org.el
+++ scimax-org.el
@@ -1,16 +1,31 @@
 ;;; scimax-org.el --- org-mode configuration for scimax	-*- lexical-binding:t ; -*-
 
-;;; Commentary:
+;; Copyright (C) 2017-2018 John Kitchin
 
+;; Author:
+;; URL:
+;; Keywords: org
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.4") (avy "0.4.0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
 
 ;;; Code:
 (require 'org)
-;; (require 'ox-latex)
-;; (require 'org-inlinetask)
-;; (require 'org-mouse)
-;; (require 'org-ref)
-;; (require 'org-agenda)
-;; (require 'dash)
+(require 'avy)
 
 
 (defun scimax-org-teleport (&optional arg)
